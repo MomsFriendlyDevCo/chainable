648d4ee55add8ba947e4caf8886e256383f163f2
Matt Carter <m@ttcarter.com>
2024-09-03 15:54:00 +1000

    task: Add build profile

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,6 @@ export default [
 		// Global ignore rules - Do not add any other keys to this object or eslint doesn't treat this as global
 		ignores: [
 			'.*',
-			'docs/',
 			'dist/',
 			'node_modules/',
 		],
